33e32e152e66ec2d8a9b672d6f9150762795b237
20100 <hodasammiir@gmail.com>
2023-05-06 17:16:55 +0300

    option delete item

--- src/OrderClasses/ShoppingCart.java
+++ src/OrderClasses/ShoppingCart.java
@@ -61,31 +61,54 @@ public class ShoppingCart {
     }
     public void updateCartItem() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the name of the item you want to update its Quantity: ");
-        String name = scanner.nextLine();
-        int quantity = 0;
+        int choice =0;
         while (true) {
-            System.out.println("Enter the Quantity you want -maximum 50-: ");
-            quantity = scanner.nextInt();
-            if (quantity <= 50) {
+            System.out.println("What Do You Want To Update ?");
+            System.out.println("1 : Discard item");
+            System.out.println("2 : Update item quantity");
+            choice = scanner.nextInt();
+            if (choice == 1 || choice == 2) {
                 break;
             }
-            System.out.println("quantity should not exceed 50!! ");
+            System.out.println("OPPS ! Invalid Choice , Please Re-enter your choice");
         }
-        for (CartItem cartItem : cartItems) {
-            if (Objects.equals(cartItem.getName(), name)) {
-                int oldQuantity = cartItem.getQuantity();
-                double oldCost = (cartItem.getPrice() - cartItem.getPrice() * (cartItem.getDiscountPercentage() / 100))
-                        * oldQuantity;
-                cartItem.setQuantity(quantity);
-                double newCost = (cartItem.getPrice() - cartItem.getPrice() * (cartItem.getDiscountPercentage() / 100))
-                        * quantity;
-                totalCost += newCost - oldCost;
-                loyaltyPoints += cartItem.getPoints() * (quantity - oldQuantity);
-                pointsEarned += (cartItem.getPrice() * LoyaltyScheme.getPointsEarnedperEgp())
-                        * (quantity - oldQuantity);
-                if(pointsEarned > LoyaltyScheme.getMaximumpoint()){
-                    pointsEarned = LoyaltyScheme.getMaximumpoint();
+        if(choice == 1){
+            System.out.println("Enter the name of the item you want to Discard: ");
+            String namee = new Scanner(System.in).nextLine();
+            for (CartItem cartItem : cartItems) {
+                if (Objects.equals(cartItem.getName(), namee)) {
+                   removeCartItem(cartItem);
+                   break;
+
+                }}
+
+        }else {
+            System.out.println("Enter the name of the item you want to update its Quantity: ");
+            String name = scanner.nextLine();
+            int quantity = 0;
+            while (true) {
+                System.out.println("Enter the Quantity you want -maximum 50-: ");
+                quantity = scanner.nextInt();
+                if (quantity <= 50) {
+                    break;
+                }
+                System.out.println("quantity should not exceed 50!! ");
+            }
+            for (CartItem cartItem : cartItems) {
+                if (Objects.equals(cartItem.getName(), name)) {
+                    int oldQuantity = cartItem.getQuantity();
+                    double oldCost = (cartItem.getPrice() - cartItem.getPrice() * (cartItem.getDiscountPercentage() / 100))
+                            * oldQuantity;
+                    cartItem.setQuantity(quantity);
+                    double newCost = (cartItem.getPrice() - cartItem.getPrice() * (cartItem.getDiscountPercentage() / 100))
+                            * quantity;
+                    totalCost += newCost - oldCost;
+                    loyaltyPoints += cartItem.getPoints() * (quantity - oldQuantity);
+                    pointsEarned += (cartItem.getPrice() * LoyaltyScheme.getPointsEarnedperEgp())
+                            * (quantity - oldQuantity);
+                    if (pointsEarned > LoyaltyScheme.getMaximumpoint()) {
+                        pointsEarned = LoyaltyScheme.getMaximumpoint();
+                    }
                 }
             }
         }
